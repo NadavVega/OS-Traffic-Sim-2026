5bf0fb28dadfe72600dd51c6750921670c98b0bd
barlahat <balahat144@gmail.com>
2026-05-23 19:41:36 +0300

    finished implementing number of travelers in parser

--- src/parser.h
+++ src/parser.h
@@ -2,7 +2,16 @@
 #define PARSER_H
 
 #include "graph.h"
+#include <sys/types.h> // For pid_t type
 
-Graph *parse_graph_from_file(const char *filename, int *src, int *dest);
+// Data structure definition for a single traveler
+typedef struct {
+    int src;   // Source node
+    int dest;  // Destination node
+    pid_t pid; // Child process ID, managed in milestone 4
+} Traveler;
 
-#endif
+// Updated function signature receiving a double pointer to the travelers array and a pointer to their count
+Graph *parse_graph_from_file(const char *filename, Traveler **travelers, int *num_travelers);
+
+#endif // PARSER_H
