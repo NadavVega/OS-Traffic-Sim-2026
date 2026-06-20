d3a807f17c86a17be0564e5c4d54b7a49d7fb1ad
barlahat <balahat144@gmail.com>
2026-06-20 18:27:17 +0300

    Remove cmake-build-debug folder from tracking

--- cmake-build-debug/_deps/raylib-build/CTestTestfile.cmake
+++
@@ -1,7 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: C:/Users/balah/Desktop/OS Project/OS-Traffic-Sim-2026/cmake-build-debug/_deps/raylib-src
-# Build directory: C:/Users/balah/Desktop/OS Project/OS-Traffic-Sim-2026/cmake-build-debug/_deps/raylib-build
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-subdirs("raylib")
