ff72390bf3b6089805235abea5a2b132ecc162d8
Nadav_vega <nadavbega@gmail.com>
2026-06-20 21:33:35 +0300

    separate milestone 5 and 6 child process signatures

--- src/child.h
+++ src/child.h
@@ -5,12 +5,15 @@
 #define MILESTONE 4
 #endif
 
-#if MILESTONE >= 5
+#if MILESTONE == 5
 #include "graph.h"
-#include <semaphore.h>
 
-// Updated to receive the sem_t array from Binyamin's API
-void run_child_process(Graph *graph, int src, int dest, int write_fd, sem_t *semaphores);
+void run_child_process(Graph *graph, int src, int dest, int write_fd);
+#elif MILESTONE >= 6
+#include "graph.h"
+
+void run_child_process(Graph *graph, int src, int dest, int write_fd,
+                       int semaphore_id);
 #else
 // The main function that will be executed by each child process
 void run_child_process(void);
